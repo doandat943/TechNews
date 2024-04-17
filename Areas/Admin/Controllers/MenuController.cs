3ad0f69e6e20ec6d4a15e23d0948ca36cbb2011e
kinhon11 <suribui27@gmail.com>
2024-04-17 14:59:39 +0700

    them menu

--- Areas/Admin/Controllers/MenuController.cs
+++ Areas/Admin/Controllers/MenuController.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechNews.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TechNews.Areas.Admin.Controllers
 {
-    public class MenuController
+    [Area("Admin")]
+    public class MenuController : Controller
     {
+        private class readonly DataContext _context;
+        public MenuController( DataContext context){
+            _context = context;
+        }
+        public IActionResult Index(){
+            var mnList = _context.Menus.OderBy(m => m.MenuID).ToList ();
+            return View(mnList);
+        }
         
     }
 }
