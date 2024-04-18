d892459c8a55630df480ed12d0e4b162f5b010ca
kinhon11 <suribui27@gmail.com>
2024-04-18 09:50:23 +0700

    xoa menu

--- Areas/Admin/Controllers/MenuController.cs
+++ Areas/Admin/Controllers/MenuController.cs
@@ -16,6 +16,25 @@ namespace TechNews.Areas.Admin.Controllers
             var mnList = _context.Menus.OrderBy(m => m.MenuId).ToList ();
             return View(mnList);
         }
+        public IActionResult Delete (int? id)
+        {
+            if (id == null || id ==0)
+               return NotFound();
+            var mn = _context.Menus.Find(id);
+            if (mn == null)
+               return NotFound();
+            return View(mn);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var delMenu = _context.Menus.Find(id);
+            if (delMenu == null)
+                return NotFound();
+            _context.Menus.Remove(delMenu);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
         
     }
 }
