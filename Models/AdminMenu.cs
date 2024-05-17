54d9a806350114371a74b3d0547211bd3ea82cae
doandat943 <doandat943@joverse.me>
2024-05-17 21:40:17 +0700

    Thêm target cho model Admin Menu

--- Models/AdminMenu.cs
+++ Models/AdminMenu.cs
@@ -10,6 +10,7 @@ namespace TechNews.Models
         public bool IsActive { get; set; }
         public string? ControllerName { get; set; }
         public string? ActionName { get; set; }
+        public string? Target { get; set; }
         public int Level { get; set; }
         public int? ParentId { get; set; } // Nullable to represent root menu items
         public int MenuOrder { get; set; }
