6bccb6b5bfd329a4fea13f2bfd32e410fa3ebd8c
doandat943 <doandat943@joverse.us>
2024-05-22 14:24:59 +0700

    Thêm xem chi tiết bài viết

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,15 +15,31 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var postList = (from post in _context.Post
+                        join menu in _context.Menu on post.MenuId equals menu.MenuId
+                        //where (m.IsActive == true)
+                        select new
+                        {
+                            Post = post,
+                            Menu = menu
+                        }).ToList();
+        return View(postList);
     }
 
+    [Route("/post-{slug}-{id:int}.html")]
     public IActionResult Detail(int? id)
     {
         if (id == null) return NotFound();
-        var post = _context.Post.FirstOrDefault(m => (m.PostId == id) && (m.IsActive == true));
-        if (post == null) return NotFound();
-        return View(post);
+        var thispost = (from post in _context.Post
+                        join menu in _context.Menu on post.MenuId equals menu.MenuId
+                        where (post.PostId == id && post.IsActive == true)
+                        select new
+                        {
+                            Post = post,
+                            Menu = menu
+                        }).ToList();
+        if (thispost == null) return NotFound();
+        return View(thispost);
     }
 
     public IActionResult Privacy()
--- Ultilities/Functions.cs
+++ Ultilities/Functions.cs
@@ -10,29 +10,47 @@ namespace TechNews.Ultilities
 {
     public class Functions
     {
-        public static string TitleSlugGeneration(string type, string? title, long id)
+        public static string TitleSlugGenerate(string type, string? title, long id)
         {
             return type + "-" + SlugGenerator.SlugGenerator.GenerateSlug(title) + "-" + id.ToString() + ".html";
         }
 
-        public static string getCurrentTime()
-        {
-            return DateTime.Now.ToString("yyyy-MM-dd HH:mm");
-        }
-
-        public static string getDiffTime(DateTime inputDate)
+        public static string GetDiffTime(DateTime inputDate, bool isShort = false)
         {
             var timeDifference = DateTime.Now - inputDate;
-            if (timeDifference.TotalDays == 0){
+            if (timeDifference.TotalDays == 0)
+            {
                 return timeDifference.TotalHours + " giờ trước";
             }
-            else if (timeDifference.TotalHours == 0){
+            else if (timeDifference.TotalHours == 0)
+            {
                 return timeDifference.TotalMinutes + " phút trước";
             }
-            else {
+            else if (isShort == true)
+            {
+                return inputDate.ToString("yyyy-MM-dd");
+            }
+            else
+            {
                 return inputDate.ToString("yyyy-MM-dd HH:mm");
             }
-            
+        }
+
+        public static string TruncateTitle(string title, int maxLength)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length <= maxLength)
+            {
+                return title;
+            }
+
+            var truncated = title.Substring(0, maxLength);
+            var lastSpaceIndex = truncated.LastIndexOf(' ');
+            if (lastSpaceIndex > 0)
+            {
+                truncated = truncated.Substring(0, lastSpaceIndex);
+            }
+
+            return truncated + "...";
         }
     }
 }
