19876ee0e78094398879bd0ed90e25c9dc82b6f6
DANG DOAN DAT <doandat943@joverse.us>
2024-05-23 15:21:36 +0000

    Bỏ ControllerName và ActionName trong bảng Menu

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -8,8 +8,6 @@ namespace TechNews.Models
         public int MenuId { get; set; }
         public string? MenuName { get; set; }
         public bool IsActive { get; set; }
-        public string? ControllerName { get; set; }
-        public string? ActionName { get; set; }
         public string? Link { get; set; }
         public int Level { get; set; }
         public int? ParentId { get; set; } // Nullable to represent root categories
