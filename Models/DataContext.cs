bdc90b3d43adb0575a02a205a6c932b425860962
doandat943 <doandat943@joverse.us>
2024-06-14 23:05:15 -0400

    Sửa lại một chút về data

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -30,15 +30,15 @@ namespace TechNews.Models
             if (!AboutUs.Any())
             {
                 AboutUs.AddRange(
-                    new AboutUs { Id = 1, WebName = "TechNews", AdminName = "Quản trị viên", AdminAvatar = "", Email = "admin@technews.com", Number = "0989123456", Address = "Vinh University", Content = "We are TechNews", Image = "", Facebook = "facebook.com", Instagram = "instagram.com", Linkedin = "linkedin.com", Twitter = "twitter.com", Youtube = "youtube.com" }
+                    new AboutUs { Id = 1, WebName = "TechNews", AdminName = "Quản trị viên", AdminAvatar = "https://i.pinimg.com/736x/cc/52/3f/cc523f7dc794dfed54aac0973348e541.jpg", Email = "admin@technews.com", Number = "0989123456", Address = "Vinh University", Content = "We are TechNews", Image = "/uploads/hoa-tua-may-troi.jpeg/", Facebook = "facebook.com", Instagram = "instagram.com", Linkedin = "linkedin.com", Twitter = "twitter.com", Youtube = "youtube.com" }
                 );
 
                 AdminMenu.AddRange(
                     new AdminMenu { MenuId = 1, MenuName = "Bảng điều khiển", RoleId = 3, ControllerName = "Home", ActionName = "Index", Level = 1, ParentId = 0, MenuOrder = 1, Target = "forms-nav", Icon = "bi bi-menu-button-wide", IsActive = true },
                     new AdminMenu { MenuId = 2, MenuName = "Quản lý", RoleId = 2, ControllerName = "Home", ActionName = "Index", Level = 1, ParentId = 0, MenuOrder = 2, Target = "components-nav", Icon = "bi bi-layout-text-window-reverse", IsActive = true },
-                    new AdminMenu { MenuId = 3, MenuName = "Quản lý bài viết", RoleId = 2, ControllerName = "Post", ActionName = "Index", Level = 2, ParentId = 1, MenuOrder = 1, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
-                    new AdminMenu { MenuId = 4, MenuName = "Quản lý danh mục", RoleId = 3, ControllerName = "Menu", ActionName = "Index", Level = 2, ParentId = 1, MenuOrder = 2, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
-                    new AdminMenu { MenuId = 5, MenuName = "Quản lý tài khoản", RoleId = 3, ControllerName = "Account", ActionName = "Index", Level = 2, ParentId = 1, MenuOrder = 3, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
+                    new AdminMenu { MenuId = 3, MenuName = "Quản lý bài viết", RoleId = 2, ControllerName = "Post", ActionName = "Index", Level = 2, ParentId = 2, MenuOrder = 1, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
+                    new AdminMenu { MenuId = 4, MenuName = "Quản lý danh mục", RoleId = 3, ControllerName = "Menu", ActionName = "Index", Level = 2, ParentId = 2, MenuOrder = 2, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
+                    new AdminMenu { MenuId = 5, MenuName = "Quản lý tài khoản", RoleId = 3, ControllerName = "Account", ActionName = "Index", Level = 2, ParentId = 2, MenuOrder = 3, Target = "", Icon = "bi bi-menu-button-wide", IsActive = true },
                     new AdminMenu { MenuId = 6, MenuName = "Quản lý tập tin", RoleId = 3, ControllerName = "FileManager", ActionName = "Index", Level = 1, ParentId = 0, MenuOrder = 3, Target = "forms-nav", Icon = "bi bi-card-list", IsActive = true },
                     new AdminMenu { MenuId = 7, MenuName = "Thông tin Website", RoleId = 3, ControllerName = "AboutUs", ActionName = "Edit", Level = 1, ParentId = 0, MenuOrder = 4, Target = "forms-nav", Icon = "bi bi-menu-button-wide", IsActive = true }
                 );
